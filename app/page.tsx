b64f394e6c7e6ffa809c9271e4800b61d59a6545
yuminnnnni <ka09023@ajou.ac.kr>
2025-07-11 22:26:29 +0900

    chore: 프로젝트 초기 설정

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-blue-500">
+      <h1 className="text-3xl font-bold text-white">
+        Tailwind v4 + App Router 작동!
+      </h1>
+      <p className="mt-4 text-lg text-white">
+        Tailwind CSS와 Next.js App Router
+      </p>
+    </main>
+  )
+}
